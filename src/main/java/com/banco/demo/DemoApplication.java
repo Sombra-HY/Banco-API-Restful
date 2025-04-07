984cca1384b11cd7d5dc3b675afacb71486540bb
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2025-04-07 09:25:15 -0300

    fiz server openAPI

--- src/main/java/com/banco/demo/DemoApplication.java
+++ src/main/java/com/banco/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.banco.demo;
 
 import com.banco.demo.model.User;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+@OpenAPIDefinition(servers = {@Server(url = "/",description = "Default Server URL")})
 @SpringBootApplication
 public class DemoApplication {
 
